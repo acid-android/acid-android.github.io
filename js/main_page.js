f32617c9b279900c3b4918b57c014b6090b2a46b
acid19912409 <murdoc50@gmail.com>
2017-02-04 13:12:51 +0200

    4th

--- js/main_page.js
+++ js/main_page.js
@@ -1,11 +1,17 @@
 var menu = $('.dropdown-menu');
 var menuButton = $('.header__menu-indicator');
 var hideButton = $('.header__menu-indicator-pushed');
+//var blur = '<div class="blur" style="position: relative; top: 0; display: block; width: 100%; filter: blur(10px);z-index: 0;"></div>';
 
 menuButton.on('click', function () {
     menuButton.hide();
     hideButton.show();
     menu.show();
+    //menu.prepend(blur);
+    $('.blur').css({
+        'height': menu.height()
+    });
+
     menu.animate({top: '+=1060'}, 1000);
 
     hideButton.on('click', function () {
@@ -58,7 +64,14 @@ function padding() {
     $('.theater-5dot1__right-group .right-group__receiver').css({
         'padding-bottom': paddingReciever5dot1 + 'px'
     });
-    console.log(paddingReciever5dot1 + ' px');
+
+    var paddingReciever7dot1 = $('.theater-7dot1__left-group').height() - $('.theater-7dot1__right-group .right-group__receiver').height() - $('.theater-7dot1__right-group .right-group__rear-surr').height();
+    $('.theater-7dot1__right-group .right-group__receiver').css({
+        'padding-bottom': paddingReciever7dot1/2 + 'px'
+    });
+    $('.theater-7dot1__right-group .right-group__rear-surr').css({
+        'padding-bottom': paddingReciever7dot1/2 + 'px'
+    });
 }
 $(window).resize(function() {
     padding();
