e4d58a77c505d8a05188d9347b31e6be5b5541e1
acid-android <murdoc50@gmail.com>
2017-08-02 11:19:41 +0300

    2.08.2017

--- js/colorSelection.js
+++ js/colorSelection.js
@@ -1,116 +1,208 @@
-$(function () {
-    var colorName = $('.info__colors .color-name span');
-    var colorButton = $('.info__colors .color');
-    var colorImage = $('.wrapper__color-image .image');
-    var activeColorButton = $('.info__colors .active');
-    var activeColorImage = $('.wrapper__color-image .show');
-    var colorOldPrice = $('.info__price .old-price span');
-    var colorPrice = $('.info__price .price span');
-    showPrice(colorOldPrice, colorPrice, activeColorButton);
-    zoom();
 
-    colorButton.on('click', function () {
-        if (!checkOnActiveColor($(this))) {
-            return;
-        }
-        var newActiveColorButton = $(this);
-        newActiveColorButton.addClass('active');
-        activeColorButton.removeClass('active');
-        activeColorButton = newActiveColorButton;
-        colorName.html(replaceUnderline(activeColorButton.data('color-name')));
-        showPrice(colorOldPrice, colorPrice, activeColorButton);
-        activeColorImage.removeClass('show');
-        var newColorImage = showColorImage(activeColorButton.data('color-name'));
-        activeColorImage = newColorImage;
-        zoom();
 
-    });
-});
 
-function replaceUnderline(data){
-    return data.replace(/\_/gi, ' ');
-}
-
-function showColorImage(data){
-    var image = $('[data-color-name="'+data+'"]');
-    image.addClass('show');
-    return image;
-}
-
-function checkOnActiveColor(colorButton){
-    if(!colorButton.hasClass('active')) {
+function checkOnActiveColor(colorButton) {
+    if (!colorButton.hasClass('active')) {
         return true;
     }
 }
 
-function showPrice(colorOldPrice, colorPrice, activeColorButton){
-    colorPrice.html(activeColorButton.data('color-price'));
-    if(activeColorButton.data('color-old-price')){
-        colorOldPrice.html(activeColorButton.data('color-old-price'));
-    }
-    //console.log('old price: ' + colorOldPrice.html() + '; new price: ' + colorPrice.html());
-    formatPrice();
 
-}
-
-function formatPrice(){
-    $('.old-price span').each(function () {
-        var value = $(this).html();
-        value = value.replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ');
-        if (value !== '')
-            $(this).html(value + ' грн');
-    });
-
-    $('.price span').each(function () {
-        var value = $(this).html();
-        value = value.replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ');
-        if (value !== '')
-            $(this).html(value + ' грн');
-    });
-}
-
-function formatLocalPrice(oldPriceSpan, priceSpan){
+function formatLocalPrice(oldPriceSpan, priceSpan) {
     var oldPriceValue = oldPriceSpan.html();
     var priceValue = priceSpan.html();
-    if(oldPriceValue){
+    if (oldPriceValue !== 'non') {
+        oldPriceSpan.show();
         oldPriceSpan.html(oldPriceValue.replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ') + ' грн');
+    } else {
+        oldPriceSpan.hide();
     }
     priceSpan.html(priceValue.replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ') + ' грн');
 }
 
-//-----------------------------COLOR SELECTION PRODUCT PAGE END-----------------------------------
+$(function () {
 
+});
 
+$(window).load(function () {
+    processProductList();
+});
 
-//-----------------------------COLOR SELECTION LIST PAGE--------------------------------------
-
-$(function processProductList() {
+function processProductList() {
+    var container = $('.container__row');
     var product = $('.row__product-wrapper');
-    product.each(function (i, elem) {
-        $(elem + '.image img').attr('src', $(elem + '.info .colors .active').data('color-path'));
-        if($(elem + '.info .colors .active').data('color-old-price')) {
-            $(elem + '.info .prices .old-price span').html($(elem + '.info .colors .active').data('color-old-price'));
-        }
-        $(elem + '.info .prices .price span').html($(elem + '.info .colors .active').data('color-price'));
-        $(elem + '.info .colors .color-name span').html(replaceUnderline($(elem + '.info .colors .active').data('color-name')));
-        formatLocalPrice($(elem + '.info .prices .old-price span'), $(elem + '.info .prices .price span'));
+    var activeColorId = '';
+    var getParams = (getAllUrlParams().sectionid ? '&sectionId=' + getAllUrlParams().sectionid : '') +
+        (getAllUrlParams().brandid ? '&brandId=' + getAllUrlParams().brandid : '') +
+        (getAllUrlParams().typeid ? '&typeId=' + getAllUrlParams().typeid : '') +
+        (getAllUrlParams().minprice ? '&minPrice=' + getAllUrlParams().minprice : '') +
+        (getAllUrlParams().maxprice ? '&maxPrice=' + getAllUrlParams().maxprice : '');
+    container.each(function (i, elem) {
+        var containerId = $(elem).attr('id');
+        var product1 = $('#' + containerId + ' .row__product-wrapper:first-child');
+        var product2 = $('#' + containerId + ' .row__product-wrapper:nth-child(2)');
+        var product3 = $('#' + containerId + ' .row__product-wrapper:nth-child(3)');
+        var limit = $('#' + containerId + ' .limit');
+        product1.each(function(i, elem){
+            var productId = $(elem).attr('id');
+            var productIdRequest = $(elem).data('product-id');
+            var elemImage = $('#' + productId + ' .image img');
+            if($(elem).attr('class') == 'row__product-wrapper cable'){
+                var href_cable = $('#' + productId + ' a');
+                href_cable.attr('href', 'http://' + window.location.host + '/hifi-site/web/product/index?id=' + productIdRequest);
+                if ($('#' + productId + ' .info .lengths .active').data('length-old-price')) {
+                    $('#' + productId + ' .info .prices .old-price span').html($('#' + productId + ' .info .lengths .active').data('length-old-price'));
+                }
+                var length = $('#' + productId + ' .info .lengths .length');
+                length.each(function (i, elem) {
+                    var lengthId = $(elem).attr('id');
+                    $('#' + lengthId + ' .checked i').html($('#' + lengthId).data('length'));
+
+                });
+                //$('#' + productId + ' .info .lengths .length .checked i').text($('#' + productId + ' .info .lengths .length').data('length'));
+                $('#' + productId + ' .info .prices .price span').html($('#' + productId + ' .info .lengths .active').data('length-price'));
+                formatLocalPrice($('#' + productId + ' .info .prices .old-price span'), $('#' + productId + ' .info .prices .price span'));
+                return;
+            }
+            activeColorId = $('#' + productId + ' .info .colors .active').data('color-id');
+            var href = $('#' + productId + ' a');
+            href.attr('href', 'http://' + window.location.host + '/hifi-site/web/product/index?id=' + productIdRequest + '&color=' + activeColorId + getParams);
+            elemImage.attr('src', $('#' + productId + ' .info .colors .active').data('color-path'));
+            if ($('#' + productId + ' .info .colors .active').data('color-old-price')) {
+                $('#' + productId + ' .info .prices .old-price span').html($('#' + productId + ' .info .colors .active').data('color-old-price'));
+            }
+            $('#' + productId + ' .info .prices .price span').html($('#' + productId + ' .info .colors .active').data('color-price'));
+
+            formatLocalPrice($('#' + productId + ' .info .prices .old-price span'), $('#' + productId + ' .info .prices .price span'));
+        });
+        product2.each(function(i, elem){
+            if ($(elem).attr('class') == 'row__product-wrapper limit') {
+                var id = $(elem).attr('id');
+                $('#' + id).height(($('#' + product1.attr('id')).height()) + 1);
+                return;
+            }
+            var productId = $(elem).attr('id');
+            var productIdRequest = $(elem).data('product-id');
+            var elemImage = $('#' + productId + ' .image img');
+            if($(elem).attr('class') == 'row__product-wrapper cable'){
+                var href_cable = $('#' + productId + ' a');
+                href_cable.attr('href', 'http://' + window.location.host + '/hifi-site/web/product/index?id=' + productIdRequest);
+                if ($('#' + productId + ' .info .lengths .active').data('length-old-price')) {
+                    $('#' + productId + ' .info .prices .old-price span').html($('#' + productId + ' .info .lengths .active').data('length-old-price'));
+                }
+                var length = $('#' + productId + ' .info .lengths .length');
+                length.each(function (i, elem) {
+                    var lengthId = $(elem).attr('id');
+                    $('#' + lengthId + ' .checked i').html($('#' + lengthId).data('length'));
+
+                });
+                $('#' + productId + ' .info .prices .price span').html($('#' + productId + ' .info .lengths .active').data('length-price'));
+                formatLocalPrice($('#' + productId + ' .info .prices .old-price span'), $('#' + productId + ' .info .prices .price span'));
+                if($('#' + productId).height() > $('#' + product1.attr('id')).height()){
+                    console.log('>');
+                    $('#' + product1.attr('id')).height($('#' + productId).height());
+                } else if($('#' + productId).height() < $('#' + product1.attr('id')).height()){
+                    $('#' + productId).height($('#' + product1.attr('id')).height() + 1);
+                }
+                return;
+            }
+            activeColorId = $('#' + productId + ' .info .colors .active').data('color-id');
+            var href = $('#' + productId + ' a');
+            href.attr('href','http://' +  window.location.host + '/hifi-site/web/product/index?id=' + productIdRequest + '&color=' + activeColorId + getParams);
+            elemImage.attr('src', $('#' + productId + ' .info .colors .active').data('color-path'));
+            if ($('#' + productId + ' .info .colors .active').data('color-old-price')) {
+                $('#' + productId + ' .info .prices .old-price span').html($('#' + productId + ' .info .colors .active').data('color-old-price'));
+            }
+            $('#' + productId + ' .info .prices .price span').html($('#' + productId + ' .info .colors .active').data('color-price'));
+            //$('#' + productId + ' .info .colors .color-name span').html(replaceUnderline($('#' + productId + ' .info .colors .active').data('color-name')));
+            formatLocalPrice($('#' + productId + ' .info .prices .old-price span'), $('#' + productId + ' .info .prices .price span'));
+            $('#' + productId).height(($('#' + product1.attr('id')).height() + 1));
+            //console.log('2nd height: ' + $('#' + productId).height());
+
+        });
+        product3.each(function(i, elem){
+            if ($(elem).attr('class') == 'row__product-wrapper limit') {
+                var id = $(elem).attr('id');
+                $('#' + id).height(($('.row__product-wrapper').first().height()) + 1);
+                return;
+            }
+            var productId = $(elem).attr('id');
+            var productIdRequest = $(elem).data('product-id');
+            var elemImage = $('#' + productId + ' .image img');
+            if($(elem).attr('class') == 'row__product-wrapper cable'){
+                var href_cable = $('#' + productId + ' a');
+                href_cable.attr('href', 'http://' + window.location.host + '/hifi-site/web/product/index?id=' + productIdRequest);
+                if ($('#' + productId + ' .info .lengths .active').data('length-old-price')) {
+                    $('#' + productId + ' .info .prices .old-price span').html($('#' + productId + ' .info .lengths .active').data('length-old-price'));
+                }
+                var length = $('#' + productId + ' .info .lengths .length');
+                length.each(function (i, elem) {
+                    var lengthId = $(elem).attr('id');
+                    $('#' + lengthId + ' .checked i').html($('#' + lengthId).data('length'));
+
+                });
+                $('#' + productId + ' .info .prices .price span').html($('#' + productId + ' .info .lengths .active').data('length-price'));
+                formatLocalPrice($('#' + productId + ' .info .prices .old-price span'), $('#' + productId + ' .info .prices .price span'));
+                if($('#' + productId).height() > $('#' + product1.attr('id')).height()){
+                    console.log('>');
+                    $('#' + product1.attr('id')).height($('#' + productId).height());
+                } else if($('#' + productId).height() < $('#' + product1.attr('id')).height()){
+                    $('#' + productId).height($('#' + product1.attr('id')).height() + 1);
+                }
+                return;
+            }
+            activeColorId = $('#' + productId + ' .info .colors .active').data('color-id');
+            var href = $('#' + productId + ' a');
+            href.attr('href', 'product?id=' + productIdRequest + '&color=' + activeColorId + getParams);
+            elemImage.attr('src', $('#' + productId + ' .info .colors .active').data('color-path'));
+            if ($('#' + productId + ' .info .colors .active').data('color-old-price')) {
+                $('#' + productId + ' .info .prices .old-price span').html($('#' + productId + ' .info .colors .active').data('color-old-price'));
+            }
+            $('#' + productId + ' .info .prices .price span').html($('#' + productId + ' .info .colors .active').data('color-price'));
+            //$('#' + productId + ' .info .colors .color-name span').html(replaceUnderline($('#' + productId + ' .info .colors .active').data('color-name')));
+            formatLocalPrice($('#' + productId + ' .info .prices .old-price span'), $('#' + productId + ' .info .prices .price span'));
+            $('#' + productId).height(($('#' + product1.attr('id')).height() + 1));
+            //console.log('3rd height: ' + $('#' + productId).height());
+        });
     });
 
     var colorButton = $('.info .colors .color');
     colorButton.on('click', function () {
-        if(!checkOnActiveColor($(this))){
+        if (!checkOnActiveColor($(this))) {
             return;
         }
         var productId = $(this).closest('.row__product-wrapper').data('product-id');
-        $('[data-product-id="'+productId+'"]' + ' .image img').attr('src', $(this).data('color-path'));
-        if($(this).data('color-old-price')) {
+
+        $('[data-product-id="' + productId + '"]' + ' .image img').attr('src', $(this).data('color-path'));
+        if ($(this).data('color-old-price')) {
             $('[data-product-id="' + productId + '"]' + ' .info .prices .old-price span').html($(this).data('color-old-price'));
         }
-        $('[data-product-id="'+productId+'"]' + ' .info .prices .price span').html($(this).data('color-price'));
-        $('[data-product-id="' + productId + '"]' + ' .info .colors .color-name span').html(replaceUnderline($(this).data('color-name')));
+        $('[data-product-id="' + productId + '"]' + ' .info .prices .price span').html($(this).data('color-price'));
+        //$('[data-product-id="' + productId + '"]' + ' .info .colors .color-name span').html(replaceUnderline($(this).data('color-name')));
         $('[data-product-id="' + productId + '"]' + ' .info .colors .active').removeClass('active');
         $(this).addClass('active');
-        formatLocalPrice($('[data-product-id="' + productId + '"]' + ' .info .prices .old-price span'), $('[data-product-id="'+productId+'"]' + ' .info .prices .price span'));
+        formatLocalPrice($('[data-product-id="' + productId + '"]' + ' .info .prices .old-price span'), $('[data-product-id="' + productId + '"]' + ' .info .prices .price span'));
+        activeColorId = $('[data-product-id="' + productId + '"]' + ' .info .colors .active').data('color-id');
+        $('[data-product-id="' + productId + '"]' + ' a').attr('href', 'product?id=' + productId + '&color=' + activeColorId + getParams);
     });
 
-});
+    var lengthButton = $('.info .lengths .length');
+
+    lengthButton.on('click', function(){
+        if (!checkOnActiveColor($(this))) {
+            return;
+        }
+        var productId = $(this).closest('.row__product-wrapper').data('product-id');
+        if ($(this).data('length-old-price')) {
+            $('[data-product-id="' + productId + '"]' + ' .info .prices .old-price span').html($(this).data('length-old-price'));
+        }
+        $('[data-product-id="' + productId + '"]' + ' .info .prices .price span').html($(this).data('length-price'));
+        //$('[data-product-id="' + productId + '"]' + ' .info .colors .color-name span').html(replaceUnderline($(this).data('color-name')));
+        $('[data-product-id="' + productId + '"]' + ' .info .lengths .active').removeClass('active');
+        $(this).addClass('active');
+        formatLocalPrice($('[data-product-id="' + productId + '"]' + ' .info .prices .old-price span'), $('[data-product-id="' + productId + '"]' + ' .info .prices .price span'));
+    });
+}
+
+
+
